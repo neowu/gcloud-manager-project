43994d19298836aa7249b6331b564919286cef2a
neo <neowu.us@gmail.com>
2021-03-23 16:37:04 -0400

    tweak mysql create user sql

--- gcloud-manager/src/main/java/core/infra/db/MySQLClient.java
+++ gcloud-manager/src/main/java/core/infra/db/MySQLClient.java
@@ -7,7 +7,6 @@ import java.io.Closeable;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.Driver;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
@@ -35,30 +34,26 @@ public class MySQLClient implements Closeable {
 
     public void createDB(String db) throws SQLException {
         logger.info("create db, db={}", db);
-        try (PreparedStatement statement = connection.prepareStatement(String.format("""
-                        CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-                """, db))) {
+        String sql = String.format("CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci", db);
+        try (var statement = connection.prepareStatement(sql)) {
             statement.execute();
         }
     }
 
     public void createUser(String user, String password, String db, List<String> privileges) throws SQLException {
         logger.info("create user, user={}", user);
-        try (PreparedStatement statement = connection.prepareStatement(String.format("""
-                        CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s'
-                """, user, password))) {
-            statement.execute();
-        }
-        try (PreparedStatement statement = connection.prepareStatement(String.format("""
-                        GRANT %s ON %s.* TO '%s'@'%%'
-                """, String.join(", ", privileges), db, user))) {
-            statement.execute();
+        try (var statement = connection.createStatement()) {
+            statement.addBatch(String.format("CREATE USER IF NOT EXISTS '%s'@'%%'", user));
+            statement.addBatch(String.format("ALTER USER '%s'@'%%' IDENTIFIED BY '%s'", user, password));
+            statement.addBatch(String.format("GRANT %s ON %s.* TO '%s'@'%%'", String.join(", ", privileges), db, user));
+            statement.executeBatch();
         }
     }
 
     @Override
     public void close() {
         if (connection != null) {
+            logger.info("close mysql connection");
             try {
                 connection.close();
             } catch (SQLException e) {
